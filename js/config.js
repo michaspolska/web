7f0a16adef14b8cceba404d9f633e70a821c5c8a
michaspolska <michaspolska@msn.com>
2026-01-17 15:12:10 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -15,6 +15,12 @@ const API_DELETE_CUSTOMER      = "https://a1.justmike.space/app/delete_customer.
 
 const API_SAVE_PRICELIST       = "https://a1.justmike.space/app/save_pricelist.pl";
 
+const API_BASE          = "https://a1.justmike.space/app";
+const API_PRICE_LISTS   = API_BASE + "/price_lists.pl";
+const API_PRICE_ITEMS   = API_BASE + "/price_items.pl";
+const API_PUBLIC_PL     = API_BASE + "/public_pricelists.pl";
+
+
 const LOGIN_PAGE               = "/login.html";
 
 /* global caches */
